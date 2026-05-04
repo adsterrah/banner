2bb44db7f03e94b500063c41b4bbd05a4d06c7bd
Irawan Ria <irawanria85@gmail.com>
2026-05-05 03:22:35 +0700

    Create ad2.js

---
+++ ad2.js
@@ -0,0 +1,12 @@
+document.write(`
+<script>
+  atOptions = {
+    'key' : '07077f5796a85a0674c4d4cd2c86d31b',
+    'format' : 'iframe',
+    'height' : 250,
+    'width' : 300,
+    'params' : {}
+  };
+<\/script>
+<script src="//deeperregardingcontend.com/07077f5796a85a0674c4d4cd2c86d31b/invoke.js"><\/script>
+`);
