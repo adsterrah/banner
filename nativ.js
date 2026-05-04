6070c2836c4df0fae2505fbed6a54059c32042c5
Irawan Ria <irawanria85@gmail.com>
2026-05-04 21:30:49 +0700

    Create nativ.js

---
+++ nativ.js
@@ -0,0 +1,4 @@
+document.write(`
+<script async="async" data-cfasync="false" src="//deeperregardingcontend.com/c6e0da3364083fde3274c0d80bdb256c/invoke.js"><\/script>
+<div id="container-c6e0da3364083fde3274c0d80bdb256c"></div>
+`);
